53dbd61c007bf813e5819fc82106d98fd7b151ef
nepp-plus <nepp.plus@gmail.com>
2022-07-09 14:53:29 +0900

    목록에 영화등수 데이터 추가 작업 중  - API기능 목록 BaseActivity에 세팅 / 활용

--- app/src/main/java/com/neppplus/dailyboxofficerank_20220709/BaseActivity.kt
+++ app/src/main/java/com/neppplus/dailyboxofficerank_20220709/BaseActivity.kt
@@ -3,14 +3,23 @@ package com.neppplus.dailyboxofficerank_20220709
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.dailyboxofficerank_20220709.api.APIList
+import com.neppplus.dailyboxofficerank_20220709.api.ServerAPI
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+//    API 기능 목록도 상속
+    lateinit var apiList : APIList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create( APIList::class.java )
+
     }
 
     abstract fun setupEvents()
--- app/src/main/java/com/neppplus/dailyboxofficerank_20220709/MainActivity.kt
+++ app/src/main/java/com/neppplus/dailyboxofficerank_20220709/MainActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.dailyboxofficerank_20220709.databinding.ActivityMainBinding
+import com.neppplus.dailyboxofficerank_20220709.datas.BasicResponse
+import com.neppplus.dailyboxofficerank_20220709.datas.MovieRankData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    val mMovieRankList = ArrayList< MovieRankData >()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,22 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        API의 요청 (임시 : 무조건 2022/01/01로 요청) => 받은 응답을 순위 목록에 담자.
+
+        apiList.getRequestDailyBoxOfficeList(
+            "2a89c2cedd0c7b1f191ea981d791e172",
+            "20220101"
+        ).enqueue( object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        } )
 
     }
 }
