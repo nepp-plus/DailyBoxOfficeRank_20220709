2e35f2656d4a84ebe26c1c297eedc4734ad2fc62
nepp-plus <nepp.plus@gmail.com>
2022-07-09 14:47:31 +0900

    리싸이클러뷰 어댑터 뼈대 작업

---
+++ app/src/main/java/com/neppplus/dailyboxofficerank_20220709/adapters/MovieRankAdapter.kt
@@ -0,0 +1,32 @@
+package com.neppplus.dailyboxofficerank_20220709.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.dailyboxofficerank_20220709.R
+import com.neppplus.dailyboxofficerank_20220709.datas.MovieRankData
+
+class MovieRankAdapter(
+    val mContext: Context,
+    val mList: ArrayList<MovieRankData>
+) : RecyclerView.Adapter<MovieRankAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.movie_rank_list_item, parent, false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
+
+}
